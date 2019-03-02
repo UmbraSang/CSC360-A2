71c3289681185b94e46190547150c77400e9955e
Connor <umbrasang@gmail.com>
2019-03-01 16:48:45 -0800

    smoke_uthreads

--- smoke_uthreads.c
+++ smoke_uthreads.c
@@ -3,7 +3,7 @@
 #include <assert.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include "pthread.h"
+#include "uthread.h"
 #include "uthread_mutex_cond.h"
 
 #define NUM_ITERATIONS 1000
@@ -15,22 +15,20 @@
 #endif
 
 struct Agent {
-  pthread_mutex_t mutex;
-  pthread_cond_t  match;
-  pthread_cond_t  paper;
-  pthread_cond_t  tobacco;
-  pthread_cond_t  smoke;
+  uthread_mutex_t mutex;
+  uthread_cond_t  match;
+  uthread_cond_t  paper;
+  uthread_cond_t  tobacco;
+  uthread_cond_t  smoke;
 };
 
 struct Agent* createAgent() {
   struct Agent* agent = malloc (sizeof (struct Agent));
-  agent->mutex  = (pthread_mutex_t)PTHREAD_MUTEX_INITIALIZER;
-  
-  agent->paper  = (pthread_cond_t)PTHREAD_COND_INITIALIZER;
-  agent->match  = (pthread_cond_t)PTHREAD_COND_INITIALIZER;
-  agent->tobacco= (pthread_cond_t)PTHREAD_COND_INITIALIZER;
-  agent->smoke  = (pthread_cond_t)PTHREAD_COND_INITIALIZER;
-  
+  agent->mutex   = uthread_mutex_create();
+  agent->paper   = uthread_cond_create (agent->mutex);
+  agent->match   = uthread_cond_create (agent->mutex);
+  agent->tobacco = uthread_cond_create (agent->mutex);
+  agent->smoke   = uthread_cond_create (agent->mutex);
   return agent;
 }
 
@@ -62,13 +60,9 @@ struct threadArgs* createThreadArgs(struct Agent* a, enum Resource b){
     return args1;
 }
 
-int matchAvail = 0;
-int paperAvail = 0;
-int tobaccoAvail = 0;
-
-pthread_cond_t matchActor     = PTHREAD_COND_INITIALIZER;
-pthread_cond_t paperActor     = PTHREAD_COND_INITIALIZER;
-pthread_cond_t tobaccoActor   = PTHREAD_COND_INITIALIZER;
+uthread_cond_t matchActor;
+uthread_cond_t paperActor;
+uthread_cond_t tobaccoActor;
 
 char* printEnum(enum Resource type){
     switch (type){
@@ -90,13 +84,13 @@ char* printEnum(enum Resource type){
 void actorChooser(){
     switch (pivot){
         case 6:
-            pthread_cond_signal(&matchActor);
+            uthread_cond_signal(matchActor);
             break;
         case 5:
-            pthread_cond_signal(&paperActor);
+            uthread_cond_signal(paperActor);
             break;
         case 3:
-            pthread_cond_signal(&tobaccoActor);
+            uthread_cond_signal(tobaccoActor);
             break;
         default:
             break;
@@ -107,25 +101,25 @@ void* resourceType(void* prepackage){
     struct threadArgs* package = prepackage;
     struct Agent* a = package->agent;
     enum Resource type = package->type;
-    pthread_mutex_lock(&a->mutex);
+    uthread_mutex_lock(a->mutex);
     switch(type){
         case MATCH:
             while(1){
-                pthread_cond_wait(&a->match, &a->mutex);
+                uthread_cond_wait(a->match);
                 pivot += type;
                 actorChooser();
             }
             break;
         case PAPER:
             while(1){
-                pthread_cond_wait(&a->paper, &a->mutex);
+                uthread_cond_wait(a->paper);
                 pivot += type;
                 actorChooser();
             }
             break;
         case TOBACCO:
             while(1){
-                pthread_cond_wait(&a->tobacco, &a->mutex);
+                uthread_cond_wait(a->tobacco);
                 pivot += type;
                 actorChooser();
             }
@@ -134,37 +128,36 @@ void* resourceType(void* prepackage){
             printf("Error has occured in ResourceType\n");
             break;
     }
-    pthread_mutex_unlock(&a->mutex);
+    uthread_mutex_unlock(a->mutex);
 }
-
 
 void smokeIt(struct Agent* a, enum Resource type){
     smoke_count[type]++;
     pivot=0;
-    pthread_cond_signal(&a->smoke);
+    uthread_cond_signal(a->smoke);
 }
 
 void* actor(void* prepackage){
     struct threadArgs* package = prepackage;
     struct Agent* a = package->agent;
     enum Resource type = package->type;
-    pthread_mutex_lock(&a->mutex);
+    uthread_mutex_lock(a->mutex);
     switch(type){
         case MATCH:
             while(1){
-                pthread_cond_wait(&matchActor, &a->mutex);
+                uthread_cond_wait(matchActor);
                 smokeIt(a, type);
             }
             break;
         case PAPER:
             while(1){
-                pthread_cond_wait(&paperActor, &a->mutex);
+                uthread_cond_wait(paperActor);
                 smokeIt(a, type);
             }
             break;
         case TOBACCO:
             while(1){
-                pthread_cond_wait(&tobaccoActor, &a->mutex);
+                uthread_cond_wait(tobaccoActor);
                 smokeIt(a, type);
             }
             break;
@@ -172,12 +165,8 @@ void* actor(void* prepackage){
             printf("Error has occured in ResourceType\n");
             break;
     }
-    pthread_mutex_unlock(&a->mutex);
+    uthread_mutex_unlock(a->mutex);
 }
-
-
-//
-
 
 /**
  * This is the agent procedure.  It is complete and you shouldn't change it in
@@ -190,46 +179,51 @@ void* agent (void* av) {
   static const int choices[]         = {MATCH|PAPER, MATCH|TOBACCO, PAPER|TOBACCO};
   static const int matching_smoker[] = {TOBACCO,     PAPER,         MATCH};
   
-  pthread_mutex_lock (&a->mutex);
-  int i;
-    for (i = 0; i < NUM_ITERATIONS; i++) {
+  uthread_mutex_lock (a->mutex);
+    for (int i = 0; i < NUM_ITERATIONS; i++) {
       int r = random() % 3;
       signal_count [matching_smoker [r]] ++;
       int c = choices [r];
       if (c & MATCH) {
         VERBOSE_PRINT ("match available\n");
-        pthread_cond_signal (&a->match);
+        uthread_cond_signal (a->match);
       }
       if (c & PAPER) {
         VERBOSE_PRINT ("paper available\n");
-        pthread_cond_signal (&a->paper);
+        uthread_cond_signal (a->paper);
       }
       if (c & TOBACCO) {
         VERBOSE_PRINT ("tobacco available\n");
-        pthread_cond_signal (&a->tobacco);
+        uthread_cond_signal (a->tobacco);
       }
       VERBOSE_PRINT ("agent is waiting for smoker to smoke\n");
-      pthread_cond_wait (&a->smoke, &a->mutex);
+      uthread_cond_wait (a->smoke);
     }
-  pthread_mutex_unlock (&a->mutex);
+  uthread_mutex_unlock (a->mutex);
   return NULL;
 }
 
 int main (int argc, char** argv) {
-  pthread_t t[7];
+  uthread_t t[7];
+  uthread_init (7);
   struct Agent*  a = createAgent();
-    int i;
-    
-    pthread_create(&t[1], NULL, actor, createThreadArgs(a, MATCH));
-    pthread_create(&t[2], NULL, actor, createThreadArgs(a, PAPER));
-    pthread_create(&t[3], NULL, actor, createThreadArgs(a, TOBACCO));
-    pthread_create(&t[4], NULL, resourceType, createThreadArgs(a, MATCH));
-    pthread_create(&t[5], NULL, resourceType, createThreadArgs(a, PAPER));
-    pthread_create(&t[6], NULL, resourceType, createThreadArgs(a, TOBACCO));
-    pthread_create(&t[0], NULL, agent, a);
 
-    pthread_join(t[0], NULL);
+  matchActor = uthread_cond_create(a->mutex);
+  paperActor= uthread_cond_create(a->mutex);
+  tobaccoActor=uthread_cond_create(a->mutex);
+  // TODO
+    t[1]=uthread_create(actor, createThreadArgs(a, MATCH));
+    t[2]=uthread_create(actor, createThreadArgs(a, PAPER));
+    t[3]=uthread_create(actor, createThreadArgs(a, TOBACCO));
+    t[4]=uthread_create(resourceType, createThreadArgs(a, MATCH));
+    t[5]=uthread_create(resourceType, createThreadArgs(a, PAPER));
+    t[6]=uthread_create(resourceType, createThreadArgs(a, TOBACCO));
+    //t[0]=uthread_create( agent, a);
 
+    //pthread_join(t[0], NULL);
+  //
+
+  uthread_join (uthread_create (agent, a), 0);
   assert (signal_count [MATCH]   == smoke_count [MATCH]);
   assert (signal_count [PAPER]   == smoke_count [PAPER]);
   assert (signal_count [TOBACCO] == smoke_count [TOBACCO]);
